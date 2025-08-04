c0c379b3777ab55d56dc5808fe1d8ab397afbc36
opppo <164714162+foAEHFo@users.noreply.github.com>
2025-08-04 14:17:42 +0800

    Update db.js

--- HM_Music/servers-koa2/config/db.js
+++ HM_Music/servers-koa2/config/db.js
@@ -1,8 +1,8 @@
 //这里放数据库配置代码
 module.exports = {
-    database:'hm_music', // 数据库名称
-    username:'root', // 用户名
-    password:'20050515ogy', // 密码
+    database:'xxxx', // 数据库名称
+    username:'xxxx', // 用户名
+    password:'xxxxx', // 密码
     option:{
         dialect:'mysql', // 数据库使用mysql
         host:'127.0.0.1' // 数据库地址
